ec8386de01dc50663efc31a81d74d634a4f8665d
LeeHanYeong <dev@lhy.kr>
2018-08-15 14:32:08 +0900

    script_merge추가

---
+++ script_merge.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export GIT_COMMITTER_EMAIL="dev@lhy.kr"
+export GIT_COMMITTER_NAME="LeeHanYeong"
+
+repo_temp=$(mktemp -d)
+git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+
+cd "$repo_temp"
+
+git checkout master
+git merge --ff-only "$TRAVIS_COMMIT"
+
+push_uri="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO"
+
+git push "$push_uri" master >/dev/null 2>&1
+#git push "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
